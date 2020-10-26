6c3066eab35e99aadd0ec0ce6705513b164a9638
Mark Morgan <github@mark-morgan.com>
2020-10-26 17:08:42 +0000

    initial commit

---
+++ src/Helper/ServiceDispatcher.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * District5 - Service Dispatch Helper
+ *
+ * @copyright District5
+ *
+ * @author District5
+ * @link https://www.district5.co.uk
+ *
+ * @license This software and associated documentation (the "Software") may not be
+ * used, copied, modified, distributed, published or licensed to any 3rd party
+ * without the written permission of District5 or its author.
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all licensed copies of the Software.
+ *
+ */
+namespace District5\Helper;
+
+class ServiceDispatcher
+{
+
+    public static function GetService(string $baseDomain, array $serviceMap) : ?string
+    {
+        $baseDomainParts = explode('.', $baseDomain);
+
+        $subdomain = join('.', explode('.', $_SERVER['HTTP_HOST'], 0 - count($baseDomainParts)));
+
+        if (!array_key_exists($subdomain, $serviceMap))
+        {
+            return null;
+        }
+
+        return $serviceMap[$subdomain];
+    }
+}
